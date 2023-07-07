cd60b80b34004f1cf4c2603f4abba04f13b2f221
Gia Huy Tran <tonytran0314@gmail.com>
2023-07-07 17:50:05 +0700

    api version

--- budget_management_app/routes/api.php
+++ budget_management_app/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\budget\BudgetController;
+use App\Http\Controllers\API\V1\budget\BudgetController;
 
 /*
 |--------------------------------------------------------------------------
